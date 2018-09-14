a97f29ec1e82ae25112876837eba0b4aeccd5126
Diogo Campos <diogocampos@users.noreply.github.com>
2018-09-14 11:04:01 -0300

    Stub `gaussJordan` function

--- rectify.js
+++ rectify.js
@@ -1,7 +1,35 @@
-async function rectifyImage(imageData, corners) {
-  // TODO: Aplicar homografia planar à região selecionada
-  console.log(corners)
+const A4 = 210 / 297
+
+async function rectifyImage(imageData, corners, ratio = A4) {
+  const width = Math.max(
+    distance(corners[0], corners[1]),
+    distance(corners[2], corners[3])
+  )
+  const height = width / ratio
+
+  const H = findHomography(corners, width, height)
+  console.log(H)
+
+  //...
+
   return imageData
+}
+
+function findHomography(fromCorners, width, height) {
+  const toCorners = [[0, 0], [width, 0], [width, height], [0, height]]
+  const system = []
+  for (let i = 0; i < 4; i++) {
+    const [x, y] = fromCorners[i]
+    const [u, v] = toCorners[i]
+    system.push([x, y, 1, 0, 0, 0, -u * x, -u * y, u])
+    system.push([0, 0, 0, x, y, 1, -v * x, -v * y, v])
+  }
+  return gaussJordan(system)
+}
+
+function gaussJordan(system) {
+  // TODO: aplicar eliminação de Gauss-Jordan, retornar solução
+  return system.map(row => row[row.length - 1])
 }
 
 function distance([x1, y1], [x2, y2]) {
