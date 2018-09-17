01fc89d7cd41912822a7755aaae66b68609eab2f
Diogo Campos <diogocampos@users.noreply.github.com>
2018-09-17 16:05:42 -0300

    Use bilinear interpolation instead of nearest-neighbor

--- rectify.js
+++ rectify.js
@@ -46,8 +46,23 @@ async function applyHomography(imageData, H, width, height) {
 }
 
 function getInterpolatedPixel(imageData, x, y) {
-  // TODO: usar uma técnica melhor de interpolação
-  return getPixel(imageData, Math.round(x), Math.round(y))
+  // https://en.wikipedia.org/wiki/Bilinear_interpolation
+
+  const x1 = Math.floor(x), x2 = x1 + 1, dx1 = x - x1, dx2 = x2 - x
+  const y1 = Math.floor(y), y2 = y1 + 1, dy1 = y - y1, dy2 = y2 - y
+
+  const p11 = getPixel(imageData, x1, y1)
+  const p21 = getPixel(imageData, x2, y1)
+  const p12 = getPixel(imageData, x1, y2)
+  const p22 = getPixel(imageData, x2, y2)
+
+  const pixel = [0, 0, 0, 0]
+  for (let i = 0; i < 4; i++) {
+    const fy1 = dx2 * p11[i] + dx1 * p21[i]
+    const fy2 = dx2 * p12[i] + dx1 * p22[i]
+    pixel[i] = Math.round(dy2 * fy1 + dy1 * fy2)
+  }
+  return pixel
 }
 
 function getPixel(imageData, x, y) {
@@ -67,6 +82,8 @@ function setPixel(imageData, x, y, rgba) {
  */
 
 function gaussJordan(system) {
+  // https://en.wikipedia.org/wiki/Gaussian_elimination
+
   for (let i = 0, len = system.length; i < len; i++) {
     const j = system.slice(i).findIndex(row => row[i] !== 0)
     if (j > 0) swapRows(system, i, i + j)
